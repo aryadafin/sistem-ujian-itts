a44ae3673fc74e2a17fbed15dfa50db7bea3eae5
aryadafin <aryadafinlzr@gmail.com>
2024-11-20 23:30:48 +0700

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF10C3C0),
+          toolbarHeight: 100, // Tinggi AppBar
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/itts.png',
+                height: 80,
+                width: 80,
+              ),
+              SizedBox(width: 10), // Jarak antara logo dan teks
+              Text(
+                "Ujian Online ITTS",
+                style: TextStyle(
+                  color: Color(0xFFF6F9FF),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Text("Coba dulu p"),
+        ),
+      ),
     );
   }
 }
